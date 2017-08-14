da8e776d8c7d2ef1948339466f5da8d69e45e5d8
Nicolas Beguier <nicolas.beguier@schibsted.com>
2017-08-14 17:56:38 +0200

    Clean demo

--- demo/launch_server.sh
+++ demo/launch_server.sh
@@ -6,9 +6,15 @@ echo 'Downloading last version of Postgresql'
 docker pull postgres
 
 echo 'Starting Postgresql server'
-docker run --rm -p 5432:5432 --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword postgres &
+docker run --rm -p 5432:5432 --name demo-postgres -e POSTGRES_PASSWORD=mysecretpassword postgres &
 
 sleep 10
 
 echo "Initialize server database"
 python server/init_pg.py
+
+sleep 5
+
+echo 'Starting CA-SSH demo server'
+docker run -it --rm -p 8080:8080 nbeguier/cassh-server
+
--- demo/server_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'START : LBC - SSH'
-
-echo 'Starting server'
-python server/server.py --ca $1 --krl $2
