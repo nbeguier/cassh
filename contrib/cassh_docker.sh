49e31db14816d77f3e5cf65700d115d895f64048
Xavier Krantz <xakraz@gmail.com>
2018-09-11 12:26:58 +0200

    [contrib] Added System service and timer files to schedule CASSH SIGN for user, every morning

--- contrib/cassh_docker.sh
+++ contrib/cassh_docker.sh
@@ -1,10 +1,44 @@
 #!/usr/bin/env bash
+#
+# To be installed as ~/.local/bin/cassh
+#
 
-docker run --rm -it \
+# == Vars
+#
+# Docker Hub
+REGISTRY='leboncoin'
+IMAGE_NAME='cassh'
+IMAGE_VERSION="${CASSH_VERSION:-latest}"
+
+CASSH_CONFIG=${CASSH_CONFIG:-${HOME}/.cassh}
+
+
+# == Bash options
+#
+set -o nounset
+
+
+# == Run
+#
+LDAP_PASSWORD=$(zenity --password --title="CASSH - sign" --text="Enter your AD/LDAP password" --timeout=30 2>/dev/null)
+
+OUTPUT=$(
+docker run --rm -i \
   --log-driver="none" \
   -u $(id -u) \
   -e HOME=${HOME} \
-  --volume=${HOME}/.cassh:${HOME}/.cassh:ro \
+  --volume=${CASSH_CONFIG}:${HOME}/.cassh:ro \
   --volume=${HOME}/.ssh:${HOME}/.ssh \
   --workdir=${HOME} \
-  leboncoin/cassh "$@"
+  ${REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION} "$@" 2>/dev/null <<< $(echo $LDAP_PASSWORD)
+)
+
+if [[ $? -eq 0 ]]; then
+  zenity --info \
+         --title="CASSH - sign"\
+         --text "${OUTPUT}"
+else
+  zenity --error \
+         --title="CASSH - sign"\
+         --text "${OUTPUT}"
+fi
